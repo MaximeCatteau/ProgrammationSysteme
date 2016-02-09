ff8d852abca16191a92d2f86ba770a9521f003b7
Maxime Catteau <maxime.catteau13@gmail.com>
2016-02-09 10:30:56 +0100

    Ce commit permet de tuer les processus zombie restants

--- webserver/socket.c
+++ webserver/socket.c
@@ -11,15 +11,32 @@
 
 /** Ignorer SIGPIPE */
 
-void initialiser_signaux(void){
-  if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
-    perror("Erreur dans le SIGPIPE\n");
-  }
-  else {
-    printf("SIGPIPE OK\n");
-  }
+void traitement_signal(int sig){
+
+  printf("Signal %d recu\n", sig);
+  
 }
 
+void initialiser_signaux(void){
+
+  
+  //Avant
+  /*if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
+    perror("Erreur dans le SIGPIPE\n");
+    }*/
+
+  //Apres
+
+  struct sigaction sa;
+
+  sa.sa_handler = traitement_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  
+  if(sigaction(SIGCHLD, &sa, NULL) == -1){
+    perror("Erreur dans le sigaction(SIGCHLD)\n");
+  }
+}
 
 int creer_serveur(int port){
 
